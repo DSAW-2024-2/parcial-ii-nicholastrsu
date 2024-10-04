169676fb2354d612d97d6292b7e80908cf86f860
nicholas <nicholastrsu@unisabana.edu.co>
2024-10-04 08:41:24 -0500

    vrcel

--- servers.js
+++ servers.js
@@ -13,7 +13,5 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Ruta no encontrada' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+module.exports = app;
+
